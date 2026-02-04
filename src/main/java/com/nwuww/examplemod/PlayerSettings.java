18e0b6c68c463bf6a75351bc5c5d23af06f42657
Nwuww <wunanfred2025@163.com>
2026-02-04 19:26:15 +0000

    新增Home相关命令

--- src/main/java/com/nwuww/examplemod/PlayerSettings.java
+++ src/main/java/com/nwuww/examplemod/PlayerSettings.java
@@ -1,9 +1,16 @@
 package com.nwuww.examplemod;
-
+class Home
+{
+    public double x;
+    public double y;
+    public double z;
+    public String dimension;
+}
 public class PlayerSettings
 {
     private boolean autoAccept = false;
     private int timeCancel = 60;
+    private final Home home = new Home();
     public boolean isAutoAccept()
     {
         return autoAccept;
@@ -20,4 +27,31 @@ public class PlayerSettings
     {
         this.timeCancel = timeCancel;
     }
+    public void setHome(double x, double y, double z, String dimension)
+    {
+        home.x = x;
+        home.y = y;
+        home.z = z;
+        home.dimension = dimension;
+    }
+    public Home getHome()
+    {
+        return home;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "====================" +
+                "\n你的当前设置: " +
+                "\n自动接受请求 = " + autoAccept +
+                "\n超时时间 = " + timeCancel + " seconds" +
+                "\n家位置 (" +
+                (int)home.x +
+                ", " + (int)home.y +
+                ", " + (int)home.z +
+                ": " + home.dimension + ")" +
+                "\n====================";
+    }
+
 }
--- src/main/java/com/nwuww/examplemod/TPCommand.java
+++ src/main/java/com/nwuww/examplemod/TPCommand.java
@@ -3,8 +3,11 @@ import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.commands.arguments.EntityArgument; // 用于解析玩家/实体
@@ -19,140 +22,239 @@ public class TPCommand
         // 命令处理
         dispatcher.register(
                 Commands.literal("tpa")
+                        .executes(context -> {
+                            var source = context.getSource().getPlayerOrException();
+                            source.sendSystemMessage(Component.literal("用法: /tpa <玩家> 或 /tpa config <设置项>"));
+                            return Command.SINGLE_SUCCESS;
+                        })
                         .then(Commands.argument("target", EntityArgument.player())
-                                .executes(context -> {
-                                    ServerPlayer source = context.getSource().getPlayerOrException();
-                                    ServerPlayer target = EntityArgument.getPlayer(context, "target");
-
-                                    if(ConfigManager.getPlayerSettings(target.getUUID()).isAutoAccept())
-                                    {
-                                        source.teleportTo(
-                                                target.serverLevel(),
-                                                target.getX(),
-                                                target.getY(),
-                                                target.getZ(),
-                                                target.getYRot(),
-                                                target.getXRot()
-                                        );
-
-                                        source.sendSystemMessage(Component.literal("传送请求已被自动接受！"));
-                                        target.sendSystemMessage(Component.literal(source.getScoreboardName() + " 已传送到你的位置！"));
-                                        return Command.SINGLE_SUCCESS;
-                                    }
-
-                                    long remainingCooldown = TpaManager.getRemainingCooldown(source.getUUID());
-                                    if (remainingCooldown > 0)
-                                    {
-                                        source.sendSystemMessage(Component.literal("还需要等待 " + remainingCooldown + " 秒后才能发送另一个传送请求").withColor(0xFF8C00));        ;
-                                        return 0;
-                                    }
-
-                                    TpaManager.addRequest(source.getUUID(), target.getUUID(), ConfigManager.getPlayerSettings(target.getUUID()).getTimeCancel());
-                                    TpaManager.cooldown(source.getUUID());
-
-                                    target.sendSystemMessage(buildRequestMessage(source.getScoreboardName()));
-                                    source.sendSystemMessage(Component.literal("传送请求已发送到" + target.getScoreboardName() + "."));
-
-                                    return Command.SINGLE_SUCCESS;
-                                })
+                                .executes(TPCommand::TpaToPlayer)
                         )
                         .then(Commands.literal("config")
+                                .executes(context ->{
+                                    var source = context.getSource().getPlayerOrException();
+                                    String configs = ConfigManager.getPlayerSettings(source.getUUID()).toString();
+                                    source.sendSystemMessage(Component.literal(configs));
+                                    return Command.SINGLE_SUCCESS;
+                                })
                                 .then(Commands.literal("autoAccept")
+                                        .executes(context ->{
+                                            var source = context.getSource().getPlayerOrException();
+                                            boolean configs = ConfigManager.getPlayerSettings(source.getUUID()).isAutoAccept();
+                                            source.sendSystemMessage(Component.literal("当前自动接受传送请求设置为: " + configs));
+                                            return Command.SINGLE_SUCCESS;
+                                        })
                                         .then(Commands.argument("value", com.mojang.brigadier.arguments.BoolArgumentType.bool())
-                                                .executes(context -> {
-                                                    ServerPlayer player = context.getSource().getPlayerOrException();
-                                                    boolean value = com.mojang.brigadier.arguments.BoolArgumentType.getBool(context, "value");
-
-                                                    ConfigManager.getPlayerSettings(player.getUUID()).setAutoAccept(value);
-                                                    ConfigManager.save();
-
-                                                    player.sendSystemMessage(Component.literal("自动接受传送请求已设置为: " + value));
-                                                    return Command.SINGLE_SUCCESS;
-                                                })
+                                                .executes(TPCommand::configAutoAccept)
                                         )
                                 )
                                 .then(Commands.literal("timeCancel")
+                                        .executes(context ->{
+                                            var source = context.getSource().getPlayerOrException();
+                                            int configs = ConfigManager.getPlayerSettings(source.getUUID()).getTimeCancel();
+                                            source.sendSystemMessage(Component.literal("当前请求超时时间设置为: " + configs + " 秒"));
+                                            return Command.SINGLE_SUCCESS;
+                                        })
                                         .then(Commands.argument("value", com.mojang.brigadier.arguments.IntegerArgumentType.integer())
-                                                .executes(context -> {
-                                                    ServerPlayer player = context.getSource().getPlayerOrException();
-                                                    int value = com.mojang.brigadier.arguments.IntegerArgumentType.getInteger(context, "value");
-
-                                                    ConfigManager.getPlayerSettings(player.getUUID()).setTimeCancel(value);
-                                                    ConfigManager.save();
-
-                                                    player.sendSystemMessage(Component.literal("请求超时时间已设置为: " + value));
-                                                    return Command.SINGLE_SUCCESS;
-                                                })
+                                                .executes(TPCommand::configTimeCancel)
                                         )
                                 )
                         )
         );
-
         dispatcher.register(
                 Commands.literal("tpdeny")
-                        .executes(context -> {
-                            ServerPlayer target = context.getSource().getPlayerOrException();
-                            UUID requesterUUID = TpaManager.getRequestTarget(target.getUUID());
-
-                            if (requesterUUID != null)
-                            {
-                                ServerPlayer requester = context.getSource().getServer().getPlayerList().getPlayer(requesterUUID);
-                                if (requester != null)
-                                {
-                                    requester.sendSystemMessage(Component.literal("传送请求被拒绝！").withColor(0xCD5C5C));
-                                    target.sendSystemMessage(Component.literal("你已拒绝传送请求"));
-                                }
-                                else
-                                {
-                                    target.sendSystemMessage(Component.literal("请求者已下线").withColor(0xFF8C00));
-                                }
-                                TpaManager.removeRequest(target.getUUID());
-                            }
-                            else
-                            {
-                                target.sendSystemMessage(Component.literal("你没有待处理的传送请求"));
-                            }
-                            return Command.SINGLE_SUCCESS;
-                        })
+                        .executes(TPCommand::TpDeny)
         );
-
         dispatcher.register(
                 Commands.literal("tpaccept")
-                        .executes(context -> {
-
-                            ServerPlayer target = context.getSource().getPlayerOrException();
-                            UUID requesterUUID = TpaManager.getRequestTarget(target.getUUID());
-
-                            if (requesterUUID != null)
-                            {
-                                ServerPlayer requester = context.getSource().getServer().getPlayerList().getPlayer(requesterUUID);
-                                if (requester != null)
-                                {
-                                    requester.teleportTo(
-                                            target.serverLevel(),
-                                            target.getX(),
-                                            target.getY(),
-                                            target.getZ(),
-                                            target.getYRot(),
-                                            target.getXRot()
-                                    );
-
-                                    requester.sendSystemMessage(Component.literal("传送请求已被接受！"));
-                                    target.sendSystemMessage(Component.literal("你已接受传送请求"));
-                                }
-                                else
-                                {
-                                    target.sendSystemMessage(Component.literal("请求者不在线"));
-                                }
-                                TpaManager.removeRequest(target.getUUID());
-                            }
-                            else
-                            {
-                                target.sendSystemMessage(Component.literal("你没有待处理的传送请求"));
-                            }
-                            return Command.SINGLE_SUCCESS;
-                        })
+                        .executes(TPCommand::TpAccept)
         );
+        dispatcher.register(
+                Commands.literal("sethome")
+                        .executes(TPCommand::SetHome)
+        );
+        dispatcher.register(
+                Commands.literal("tphome")
+                        .executes(TPCommand::TpHome)
+        );
+    }
+
+    // /tpa <target>
+    private static int TpaToPlayer(CommandContext<CommandSourceStack> context) throws CommandSyntaxException
+    {
+        ServerPlayer source = context.getSource().getPlayerOrException();
+        ServerPlayer target = EntityArgument.getPlayer(context, "target");
+
+        if(ConfigManager.getPlayerSettings(target.getUUID()).isAutoAccept())
+        {
+            source.teleportTo(
+                    target.serverLevel(),
+                    target.getX(),
+                    target.getY(),
+                    target.getZ(),
+                    target.getYRot(),
+                    target.getXRot()
+            );
+
+            source.sendSystemMessage(Component.literal("传送请求已被自动接受！"));
+            target.sendSystemMessage(Component.literal(source.getScoreboardName() + " 已传送到你的位置！"));
+            return Command.SINGLE_SUCCESS;
+        }
+
+        long remainingCooldown = TpaManager.getRemainingCooldown(source.getUUID());
+        if (remainingCooldown > 0)
+        {
+            source.sendSystemMessage(Component.literal("还需要等待 " + remainingCooldown + " 秒后才能发送另一个传送请求").withColor(0xFF8C00));        ;
+            return 0;
+        }
+
+        TpaManager.addRequest(source.getUUID(), target.getUUID(), ConfigManager.getPlayerSettings(target.getUUID()).getTimeCancel());
+        TpaManager.cooldown(source.getUUID());
+
+        target.sendSystemMessage(buildRequestMessage(source.getScoreboardName()));
+        source.sendSystemMessage(Component.literal("传送请求已发送到" + target.getScoreboardName() + "."));
+
+        return Command.SINGLE_SUCCESS;
+    }
+
+    // /tpa config autoAccept <value>
+    private static int configAutoAccept(CommandContext<CommandSourceStack> context) throws CommandSyntaxException
+    {
+        ServerPlayer player = context.getSource().getPlayerOrException();
+        boolean value = com.mojang.brigadier.arguments.BoolArgumentType.getBool(context, "value");
+
+        ConfigManager.getPlayerSettings(player.getUUID()).setAutoAccept(value);
+        ConfigManager.save();
+
+        player.sendSystemMessage(Component.literal("自动接受传送请求已设置为: " + value));
+        return Command.SINGLE_SUCCESS;
+    }
+
+    // /tpa config timeCancel <value>
+    private static int configTimeCancel(CommandContext<CommandSourceStack> context) throws CommandSyntaxException
+    {
+        ServerPlayer player = context.getSource().getPlayerOrException();
+        int value = com.mojang.brigadier.arguments.IntegerArgumentType.getInteger(context, "value");
+
+        ConfigManager.getPlayerSettings(player.getUUID()).setTimeCancel(value);
+        ConfigManager.save();
+
+        player.sendSystemMessage(Component.literal("请求超时时间已设置为: " + value));
+        return Command.SINGLE_SUCCESS;
+    }
+
+    // /tpdeny
+    private static int TpDeny(CommandContext<CommandSourceStack> context) throws CommandSyntaxException
+    {
+        ServerPlayer target = context.getSource().getPlayerOrException();
+        UUID requesterUUID = TpaManager.getRequestTarget(target.getUUID());
+
+        if (requesterUUID != null)
+        {
+            ServerPlayer requester = context.getSource().getServer().getPlayerList().getPlayer(requesterUUID);
+            if (requester != null)
+            {
+                requester.sendSystemMessage(Component.literal("传送请求被拒绝！").withColor(0xCD5C5C));
+                target.sendSystemMessage(Component.literal("你已拒绝传送请求"));
+            }
+            else
+            {
+                target.sendSystemMessage(Component.literal("请求者已下线").withColor(0xFF8C00));
+            }
+            TpaManager.removeRequest(target.getUUID());
+        }
+        else
+        {
+            target.sendSystemMessage(Component.literal("你没有待处理的传送请求"));
+        }
+        return Command.SINGLE_SUCCESS;
+    }
+
+    // /tpaccept
+    private static int TpAccept(CommandContext<CommandSourceStack> context) throws CommandSyntaxException
+    {
+        ServerPlayer target = context.getSource().getPlayerOrException();
+        UUID requesterUUID = TpaManager.getRequestTarget(target.getUUID());
+
+        if (requesterUUID != null)
+        {
+            ServerPlayer requester = context.getSource().getServer().getPlayerList().getPlayer(requesterUUID);
+            if (requester != null)
+            {
+                requester.teleportTo(
+                        target.serverLevel(),
+                        target.getX(),
+                        target.getY(),
+                        target.getZ(),
+                        target.getYRot(),
+                        target.getXRot()
+                );
+
+                requester.sendSystemMessage(Component.literal("传送请求已被接受！"));
+                target.sendSystemMessage(Component.literal("你已接受传送请求"));
+            }
+            else
+            {
+                target.sendSystemMessage(Component.literal("请求者不在线"));
+            }
+            TpaManager.removeRequest(target.getUUID());
+        }
+        else
+        {
+            target.sendSystemMessage(Component.literal("你没有待处理的传送请求"));
+        }
+        return Command.SINGLE_SUCCESS;
+    }
+
+    // /sethome
+    private static int SetHome(CommandContext<CommandSourceStack> context) throws CommandSyntaxException
+    {
+        ServerPlayer source = context.getSource().getPlayerOrException();
+        ConfigManager.getPlayerSettings(source.getUUID()).setHome(
+                source.getX(),
+                source.getY(),
+                source.getZ(),
+                source.level().dimension().location().toString()
+        );
+        ConfigManager.save();
+        source.sendSystemMessage(Component.literal("家已设置到" +
+                String.format("X: %.1f Y: %.1f Z: %.1f in %s",
+                        source.getX(), source.getY(), source.getZ(), source.level().dimension().location().toString()
+                )
+        ));
+        return Command.SINGLE_SUCCESS;
+    }
+
+    // /tphome
+    private static int TpHome(CommandContext<CommandSourceStack> context) throws CommandSyntaxException
+    {
+        ServerPlayer source = context.getSource().getPlayerOrException();
+        PlayerSettings settings = ConfigManager.getPlayerSettings(source.getUUID());
+        Home home = settings.getHome();
+
+        if (home.dimension == null || home.dimension.isEmpty())
+        {
+            source.sendSystemMessage(Component.literal("你还没有设置家！").withColor(0xFF8C00));
+            return 0;
+        }
+
+        var dimensionKey =
+                net.minecraft.resources.ResourceKey.create(
+                        Registries.DIMENSION,
+                        net.minecraft.resources.ResourceLocation.parse(home.dimension)
+                );
+
+        source.teleportTo(
+                source.getServer().getLevel(dimensionKey),
+                home.x,
+                home.y,
+                home.z,
+                source.getYRot(),
+                source.getXRot()
+        );
+
+        source.sendSystemMessage(Component.literal("已传送到家！"));
+        return Command.SINGLE_SUCCESS;
     }
 
     private static Component buildRequestMessage(String requesterName)
